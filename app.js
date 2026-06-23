17db4c43e2475644e9b7a3c03c215bdb417d1905
mtsk123 <mtskhomeed@gmail.com>
2026-06-23 10:53:09 +0300

    step 3 done

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
 /*-------------- Constants --------------*/
-
+Const choicesArr = ('rock','paper','scissors')
 /*-------------- Variables --------------*/
 let userChoice = ''
-
+let computer
+let randomIndex = Math.floor(Math.random)
 /*------ Cached Element References ------*/
 const choices = document.querySelector('#choices')
 
 const rock = document.querySelector('#rock')
 const paper = document.querySelector('#paper')
-const scissors = document.querySelector('#rock')
+const scissors = document.querySelector('#scissors')
+
 const reset = document.querySelector('#reset')
-console.log(reset)
+
 /*----------- Event Listeners -----------*/
 choices.addEventListener('click', function(event){
     userChoice = event.target.id
 
-//we want to hide buttons are not userchoice
-if(userChoice === 'rock'){
-    // add hidden class to papper and scissors button
-    paper.classList.add('hidden')
-    scissors.classList.add('hidden')
-}
-
+    // we want to hide buttons that are not userChoice
+    if (userChoice === 'rock'){
+        // add hidden class to paper and scissors buttons
+        paper.classList.add('hidden')
+        scissors.classList.add('hidden')
+    } else if (userChoice === 'paper') {
+        rock.classList.add('hidden')
+        scissors.classList.add('hidden')
+    } else {
+        rock.classList.add('hidden')
+        paper.classList.add('hidden')
+    }
+computerChoice = choicesArr
     console.log('userChoice: ', userChoice)
-reset.addEventListener('click', function()){
+    
+})
+
+reset.addEventListener('click', function(){
     paper.classList.remove('hidden')
     scissors.classList.remove('hidden')
     rock.classList.remove('hidden')
-}
 
 })
-
 
 /*-------------- Functions --------------*/
